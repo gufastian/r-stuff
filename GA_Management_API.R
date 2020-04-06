adc0deba0d9bf2e27cacac97f92736bf0acb6459
Montino <montino@costa.it>
2020-04-06 10:19:15 +0200

    user removal

--- GA_Management_API.R
+++ GA_Management_API.R
@@ -1,10 +1,20 @@
 
-library(googleAnalyticsR)
+
 require(tidyverse)
 
+# ClientID: 559052943659-39i8ju2mmn0vtjrpnmiigtqrq4vqg5ah.apps.googleusercontent.com
+# Secret: tznZ9j-BlPaiBFsLY9zASPAd
+#NEW
+#348639780589-7hqvgfjcmsbm3u03e1lr146p77bu1a0s.apps.googleusercontent.com
+#_QRDstLrI1yVSkStx6vYPbOp
+
+
+GAR_CLIENT_JSON="C:/Users/montino/Downloads/client_secret_348639780589-7hqvgfjcmsbm3u03e1lr146p77bu1a0s.apps.googleusercontent.com.json"
 
 # Authorize Google Analytics. This should launch a browser and require you to
 # log in.
+googleAuthR::gar_set_client(GAR_CLIENT_JSON)
+library(googleAnalyticsR)
 ga_auth()
 
 accounts <- ga_account_list()
@@ -18,3 +28,28 @@ for (a in seq_along(uniqueAccounts$`accounts$accountId`)) {
 
 
 userList <- ga_users_list("73667627", webPropertyId = NULL, viewId = NULL); writeClipboard(unique(userList$userRef.email))
+
+
+#REMOVAL
+
+library(readxl)
+removal <- read_excel("C:/Users/montino/Downloads/removal.xlsx", col_types = c("text", "text"))
+View(removal)
+d <- 1;
+errorLog <- ""
+for (c in removal$email) {
+  cat(c," - ",removal[d,2,1],"\n")
+  res <- try(ga_users_delete(c,removal[d,2,1]))
+  if(inherits(res, "try-error"))
+  {
+    #error handling code, maybe just skip this iteration using
+    errorLog <- paste (errorLog, "ERROR;",c,";",removal[d,2,1],"\n")
+    cat("ERROR;",c,";",removal[d,2,1],"\n")
+    d <- (d+1)
+    next
+  } else {
+    d <- (d+1)
+  }
+  
+}
+
