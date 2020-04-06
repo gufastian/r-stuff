6dec7ccc20b0c09d00e5a52fe25f4e972e601bc3
Montino <montino@costa.it>
2020-04-06 14:24:17 +0200

    new vers

--- GA_Management_API.R
+++ GA_Management_API.R
@@ -1,6 +1,7 @@
 
 
 require(tidyverse)
+library(purrr)
 
 # ClientID: 559052943659-39i8ju2mmn0vtjrpnmiigtqrq4vqg5ah.apps.googleusercontent.com
 # Secret: tznZ9j-BlPaiBFsLY9zASPAd
@@ -21,13 +22,17 @@ accounts <- ga_account_list()
 uniqueAccounts <- distinct(accounts, accounts$accountId, accounts$accountName)
 
 View(uniqueAccounts)
-
+userList <- list()
 for (a in seq_along(uniqueAccounts$`accounts$accountId`)) {
-  cat(uniqueAccounts[a,1,1],";", uniqueAccounts[a,2,1], "\n");
+  if (uniqueAccounts[a,1,1] != "61448901") {
+    cat(uniqueAccounts[a,1,1],";", uniqueAccounts[a,2,1], "\n");
+    userList <- ga_users_list(uniqueAccounts[a,1,1], webPropertyId = NULL, viewId = NULL);
+    write_excel_csv(userList,path = "C:/Users/montino/Downloads/users.csv" ,append = TRUE)
+  }
 }
 
+#writeClipboard(unique(userList$userRef.email))
 
-userList <- ga_users_list("73667627", webPropertyId = NULL, viewId = NULL); writeClipboard(unique(userList$userRef.email))
 
 
 #REMOVAL
@@ -43,8 +48,18 @@ for (c in removal$email) {
   if(inherits(res, "try-error"))
   {
     #error handling code, maybe just skip this iteration using
-    errorLog <- paste (errorLog, "ERROR;",c,";",removal[d,2,1],"\n")
-    cat("ERROR;",c,";",removal[d,2,1],"\n")
+    #errorLog <- paste (errorLog, "ERROR;",c,";",removal[d,2,1],"\n")
+    
+    res <- try(ga_users_delete(c,removal[d,2,1]))
+    if(inherits(res, "try-error"))
+    {
+      #error handling code, maybe just skip this iteration using
+      errorLog <- paste (errorLog, "ERROR;",c,";",removal[d,2,1],"\n")
+      cat("ERROR;",c,";",removal[d,2,1],"\n")
+      next
+    } else {
+    }
+    
     d <- (d+1)
     next
   } else {
